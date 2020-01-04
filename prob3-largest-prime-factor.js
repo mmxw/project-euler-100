5feaf3e8db9254fa813dce2ac111c573560d9f48
cara <xw33@outlook.com>
2020-01-04 06:16:56 +0000

    problem 3: largest prime factor; 1st try timed out for 12digit number

---
+++ prob3-largest-prime-factor.js
@@ -0,0 +1,52 @@
+// Problem 3: Largest prime factor
+// The prime factors of 13195 are 5, 7, 13 and 29.
+
+// What is the largest prime factor of the given number?
+
+function largestPrimeFactor(number) {
+  let primeArr = primeUpTo(number)
+  let primeFactorArr = []
+  for (let i = primeArr.length; i >=0; i--) {
+    switch(true) {
+      case number % primeArr[i] === 0:
+        primeFactorArr.push(primeArr[i])
+        break
+      default: 
+        break
+    }
+  }
+  return primeFactorArr[0]
+}
+
+
+function primeUpTo(number) {
+  let primeArr = []
+  for (let i = 2; i <= number; i++) {
+    const numsUpToI = [...Array(i).keys()].slice(2, i)
+    switch(true) {
+      case numsUpToI.some(num => i % num === 0): 
+        break
+      default: 
+        primeArr.push(i)
+    }
+  }
+  return primeArr
+}
+
+
+
+
+console.log(largestPrimeFactor(13195))
+
+
+
+
+
+
+//* TEST
+// largestPrimeFactor(2) should return 2.
+// largestPrimeFactor(3) should return 3.
+// largestPrimeFactor(5) should return 5.
+// largestPrimeFactor(7) should return 7.
+// largestPrimeFactor(13195) should return 29.
+// largestPrimeFactor(600851475143) should return 6857. //! test time out; should figure out a better way
