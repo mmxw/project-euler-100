214ffc0ffa75560ae9fe7c5fc19af2d07300322c
cara <xw33@outlook.com>
2020-01-07 17:17:23 +0000

    refactor; solve timeout problem

--- prob3-largest-prime-factor.js
+++ prob3-largest-prime-factor.js
@@ -4,39 +4,60 @@
 // What is the largest prime factor of the given number?
 
 function largestPrimeFactor(number) {
-  let primeArr = primeUpTo(number)
-  let primeFactorArr = []
-  for (let i = primeArr.length; i >=0; i--) {
-    switch(true) {
-      case number % primeArr[i] === 0:
-        primeFactorArr.push(primeArr[i])
-        break
-      default: 
-        break
+  let i = 2
+  while (i <= number) {
+    if (number % i === 0) {
+      number /= i
+    } else {
+      i++
     }
   }
-  return primeFactorArr[0]
+  return i
 }
 
-
-function primeUpTo(number) {
-  let primeArr = []
-  for (let i = 2; i <= number; i++) {
-    const numsUpToI = [...Array(i).keys()].slice(2, i)
-    switch(true) {
-      case numsUpToI.some(num => i % num === 0): 
-        break
-      default: 
-        primeArr.push(i)
-    }
-  }
-  return primeArr
-}
+console.log(largestPrimeFactor(600851475143))
 
 
 
 
-console.log(largestPrimeFactor(13195))
+
+
+
+// function largestPrimeFactor(number) {
+//   let primeArr = primeUpTo(number)
+//   let primeFactorArr = []
+//   for (let i = primeArr.length; i >=0; i--) {
+//     switch(true) {
+//       case number % primeArr[i] === 0:
+//         primeFactorArr.push(primeArr[i])
+//         break
+//       default: 
+//         break
+//     }
+//   }
+//   return primeFactorArr[0]
+// }
+
+
+// function primeUpTo(number) {
+//   let primeArr = []
+//   for (let i = 2; i <= number; i++) {
+//     const numsUpToI = [...Array(i).keys()].slice(2, i)
+//     switch(true) {
+//       case numsUpToI.some(num => i % num === 0): 
+//         break
+//       default: 
+//         primeArr.push(i)
+//     }
+//   }
+//   return primeArr
+// }
+
+
+
+
+
+
 
 
 
@@ -49,4 +70,5 @@ console.log(largestPrimeFactor(13195))
 // largestPrimeFactor(5) should return 5.
 // largestPrimeFactor(7) should return 7.
 // largestPrimeFactor(13195) should return 29.
-// largestPrimeFactor(600851475143) should return 6857. //! test time out; should figure out a better way
+// largestPrimeFactor(600851475143) should return 6857. 
+//! test time outshould figure out a better 
